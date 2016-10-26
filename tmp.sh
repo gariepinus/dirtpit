eb94b84ef06411e91e66cf6d025c97621fc0f6f2
gariepinus <mail@gariepinus.de>
2016-10-26 10:36:22 +0200

    bloated tmp a bit

--- tmp.sh
+++ tmp.sh
@@ -1,33 +1,37 @@
 #! /bin/bash
-args=($@)
-server="minsk:.tmp"
+VERSION=0.2
 
-help="\n
+
+HELP="\n
 ################################################################################\n
-# tmp                                                                           \n
+# tmp v$VERSION                                                                 \n
 #  [quick and dirty string exchange]                                            \n
 #                                                                               \n
 # by gariepinus <mail@gariepinus.de>                                            \n
 ################################################################################\n
                                                                                 \n
  USAGE:                                                                         \n
-    tmp [command|string]                                                        \n
+    tmp [options] [command|string]                                              \n
                                                                                 \n
- Without any arguments given tmp prints the content of ~/.tmp to stdout.        \n
-                                                                                \n
- tmp takes one argument which can be either a string you want to store          \n
- or a command.                                                                  \n
+ tmp takes one parameter which can be either a string you want to store         \n
+ or a command. Without this parameter tmp prints the content of your tmp-store  \n
+ and exits.                                                                     \n
                                                                                 \n
  COMMANDS:                                                                      \n
-    tmp <string>..store string                                                  \n
-    tmp add.......append to ~/.tmp                                              \n
-    tmp stdin.....store piped input                                             \n
-    tmp put.......send ~/.tmp to server                                         \n
-    tmp get.......receive ~/.tmp from server                                    \n
-    tmp help......display this message                                          \n
+    tmp <string>. store string                                                  \n
+    tmp stdin.... store input from stdin instead of parameter                   \n
+    tmp put...... send ~/.tmp to server                                         \n
+    tmp get...... receive ~/.tmp from server                                    \n
+    tmp help..... display this message                                          \n
                                                                                 \n
-  FILES:                                                                        \n
-    ~/.tmp -- your information gets stored here                                 \n
+ OPTIONS:                                                                       \n
+    - p ...... print path to tmp-store and exit                                 \n
+    - a ...... append to tmp store instead of overwriteting                     \n
+    - H HOST.. use HOST for get/pull instead of default server                  \n
+    - v ...... verbose                                                          \n
+    - V ...... dispay version information and exit                              \n
+    - h ...... display help message and exit                                    \n
+                                                                                \n
                                                                                 \n
   LICENSE:                                                                      \n
 >>                                                                              \n
@@ -40,51 +44,187 @@ help="\n
  <<                                                                             \n
                                                                                 \n"
 
+### DEFAULTS ###
+SERVER="spock"
+VERBOSE=false
+APPEND=false
+STORE=""
+
+
+### FUNCTIONS ###
+function error {
+    echo -e $1 >&2
+    exit 1
+}
+
+function verbose {
+    if $VERBOSE
+    then
+	echo -e $1
+    fi
+}
+
+function findstore {
+    if [ -d $HOME/tmp/ ] && [ -x $HOME/tmp/ ]
+    then
+	verbose "'$HOME/tmp/' accessible."
+    localtmp=true
+    else
+	verbose "'$HOME/tmp/' NOT accessible."
+	localtmp=false
+    fi
+    if [ -d /tmp/ ] && [ -x /tmp/ ]
+    then
+	verbose "'/tmp/' accessible"
+	globaltmp=true
+    else
+	verbose "'/tmp/' NOT accessible"
+	globaltmp=false
+    fi    
+    
+    if ! $localtmp && ! $globaltmp
+    then
+	error "Neither '/tmp/' nor '$HOME/tmp/' accessible!"
+    elif $localtmp
+    then
+	if [ -e $HOME/tmp/tmp-store ]
+	then
+	    localtmp=true
+	    globaltmp=false
+	elif [ -e /tmp/tmp-store-$USER ]
+	then
+	    localtmp=false
+	    globaltmp=true
+	else
+	    localtmp=true
+	    globaltmp=false
+	fi
+    else
+	localtmp=false
+	globaltmp=true
+    fi
+    
+    if $localtmp
+    then
+	STORE=$HOME/tmp/tmp-store
+    else
+	STORE=/tmp/tmp-store-$USER
+    fi
+
+    if ! [ -e $STORE ]
+    then
+	touch $STORE
+	verbose "'$STORE' created"
+    fi
+}
+
+function findremote {
+    remote=$(ssh $SERVER tmp -p)
+    verbose "Remote path: '$remote'"
+    return $remote
+}
+
+
+### OPTIONS ###
+while getopts ":paHvVh" option ; do
+    case $option in
+	p)
+	    findstore
+	    echo $STORE
+	    exit 0
+	    ;;
+	a)
+	    APPEND=true
+	    ;;
+	H)
+	    SERVER=$OPTARG
+	    ;;
+	v)
+	    VERBOSE=true
+	    ;;
+	V)
+	    echo "Version: $VERSION"
+	    exit 0
+	    ;;
+	h)
+	    echo -e $HELP
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    echo "Try $0 -h for more information."
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    echo "Try $0 -h for more information."
+	    exit 1
+	    ;;
+    esac
+done
+
+
+### EXEC ###
+shift $((OPTIND-1))
+args=($@)
+findstore
+	
+# If no param was given, print content of tmp-store and exit
 if [ ${#args[@]} -eq 0 ]
 then
-    cat ~/.tmp
+    cat $STORE
     exit 0
 fi
 
-if [ ${#args[@]} -eq 1 ]
+# If more than one param present exit with error
+if [ ${#args[@]} -ne 1 ]
 then
-    if [ ${args[0]} == "put" ]
-    then
-	scp ~/.tmp $server
-	exit 0
-    fi
-    if [ ${args[0]} == "get" ]
-    then
-	scp $server ~/.tmp
-	cat ~/.tmp
-	exit 0
-    fi
-    if [ ${args[0]} == "help" ]
-    then
-	echo -e $help
-	exit 0
-    fi
-    if [ ${args[0]} == "add" ]
-    then
-	read input
-	echo $input >> ~/.tmp
-	cat ~/.tmp
-	exit 0
-    fi
-    if [ ${args[0]} == "stdin" ]
-    then
-	read input
-	echo $input > ~/.tmp
-	cat ~/.tmp
-	exit 0
-    fi
-
-    
-    echo ${args[0]} > ~/.tmp
-    cat ~/.tmp
-    exit 0
+    echo "Usage: tmp [options] [command|string]"
+    error "Too many parameters present, try 'tmp help'"
 fi
 
-printf "[Error] *** Too many command line arguments\n\n"
-echo -e $help
-exit 1
+# Eval command
+if [ ${args[0]} == "help" ]
+then
+    echo -e $HELP
+    exit 0
+elif [ ${args[0]} == "put" ]
+then
+    remotestore=findremote
+    if $VERBOSE
+    then
+	scp -v $STORE $SERVER:$remotestore
+    else
+	scp $STORE $SERVER:$remotestore 2>&1
+    fi
+    exit 0
+elif [ ${args[0]} == "get" ]
+then
+    remotestore=findremote
+    if $VERBOSE
+    then
+	scp -v $SERVER:$remotestore $STORE
+    else
+	scp $SERVER:$remotestore $STORE 2>&1
+    fi
+    exit 0
+elif [ ${args[0]} == "stdin" ]
+then
+    read input
+    if $APPEND
+    then
+	echo $input >> $STORE
+    else
+	echo $input > $STORE
+    fi
+    exit 0
+else
+    # If no command present, treat param as string to store
+    if $APPEND
+    then
+	echo ${args[0]} >> $STORE
+    else
+	echo ${args[0]} > $STORE
+    fi
+    exit 0
+fi
